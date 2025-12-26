99fbba7640e6549c1c5f2b7d08318f2eebb10bfc
星野 <1961335016@qq.com>
2025-12-26 12:51:19 +0800

    配置: 添加Room数据库和OkHttp网络库依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
@@ -46,4 +47,15 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+
+    // 2. 数据库 (Room - 用于“专注记录”)
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    kapt("androidx.room:room-compiler:$room_version") // 如果报错找不到kapt，看下面注脚
+    implementation("androidx.room:room-ktx:$room_version") // 支持协程
+
+    // 3. 协程 (Kotlin Coroutines - 用于倒计时)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
